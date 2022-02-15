1564ef60717057b7eabb921a217663f343ddc975
TaiFaitter <taifaitter@gmail.com>
2022-02-15 19:34:15 +0200

    Lisättiin liikkuminen ja aloitettin animaatioita

--- 2D Fighter Game/Assets/Scripts/Movement.cs
+++ 2D Fighter Game/Assets/Scripts/Movement.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
-{
-    public Rigidbody2D myRigidBody;
+{   
     
+    private Rigidbody2D myRigidBody;
+    private CircleCollider2D myFeet;
+
+    private LayerMask ground;
+
+    public float speed = 5f;
+    public float jumpForce = 7f;
+    private float horizontalMovement = 0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myFeet = GetComponent<CircleCollider2D>();
+        ground = LayerMask.GetMask("Ground");
     }
 
     // Update is called once per frame
+    // Ottaa koneen tehot huomioon p‰ivitysnopeudessa
     void Update()
     {
-        
+        horizontalMovement = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump") && myFeet.IsTouchingLayers(ground))
+        {
+            // Impulse lis‰‰ kaiken voiman kerralla, jolloin se alkaa v‰hitellen v‰henty‰
+            myRigidBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    // Ei ota huomioon koneen tehoja p‰ivitysnopeudessa
+    private void FixedUpdate()
+    {
+        myRigidBody.velocity = new Vector2(horizontalMovement * speed, myRigidBody.velocity.y);
     }
 }
